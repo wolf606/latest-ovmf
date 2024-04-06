b97d9364fa067b37a496b7a6f27293b6112e23b4
wolf606 <luismsanchez808@gmail.com>
2024-04-06 01:05:25 -0400

    Create powershell script

---
+++ build.ps1
@@ -0,0 +1,18 @@
+# Enable BuildKit
+$env:DOCKER_BUILDKIT = 1
+
+# Build Docker image
+docker build -t OvmfPkg .
+
+# Run Docker container and copy binaries
+$outputDir = "$(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)\output"
+New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
+
+$containerId = docker run -d OvmfPkg
+docker cp "$containerId:/edk2/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_CODE.fd" "$outputDir\OVMF_CODE.fd"
+docker cp "$containerId:/edk2/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_VARS.fd" "$outputDir\OVMF_VARS.fd"
+
+# Stop and remove the container
+docker stop $containerId
+docker rm $containerId
+
